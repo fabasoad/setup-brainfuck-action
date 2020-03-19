cdbf56d41788ee7ee6a377ee18f8eaf6ee2fa455
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-19 23:26:23 +0900

    Comment out sudo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const version = core.getInput('version');
 
 (async () => {
   try {
-    const sudo = process.platform !== 'win32' ? 'sudo ' : '';
+    const sudo = '';// process.platform !== 'win32' ? 'sudo ' : '';
     await exec(`${sudo}pip install brainfucky` + (version ? `==${version}` : ''));
   } catch (e) {
     core.setFailed(e.message);
