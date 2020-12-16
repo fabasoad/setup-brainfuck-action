aca3c2093d628deb3bfe51031e7263952a7b11e4
fabasoad <fabasoad@gmail.com>
2020-12-16 22:43:00 +0900

    Update CI (latest) pipeline

--- __tests__/test-command.js
+++ __tests__/test-command.js
@@ -3,12 +3,17 @@ const { assert } = require('chai');
 const Command = require('../src/command');
 
 describe('Test Command class', () => {
-  const platforms = ['aix','darwin','freebsd','linux','openbsd','sunos','win32'];
-
-  itParam('should return correct command for ${value}', platforms, (platform) => {
-    const version = '9cb25a';
-    const command = new Command(version, () => platform);
-    const sudo = platform !== 'win32' ? 'sudo ' : '';
-    assert.equal(`${sudo}pip3 install brainfucky` + (version ? `==${version}` : ''), command.build());
-  });
+  itParam(
+    'should return correct command for ${value}',
+    ['aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', 'win32'],
+    (platform) => {
+      const version = '9cb25a';
+      const command = new Command(version, () => platform);
+      const sudo = platform !== 'win32' ? 'sudo ' : '';
+      assert.equal(
+        `${sudo}pip3 install brainfucky` + (version ? `==${version}` : ''),
+        command.build()
+      );
+    }
+  );
 });
